2aea4057dd0915d319811c1f021fe795e5b5ea0a
Laurie Roy <laurieroy.dev@gmail.com>
2022-01-31 14:35:16 -0800

    Add dummy data

---
+++ src/assets/messages.js
@@ -0,0 +1,8 @@
+const messages = [
+  {user: "Freddy Kreuger", message: "Starring in a new movie where I murder coders. Calling it Freddy vs. JSON", likes: 20},
+  {user: "Jason Voorhees", message: "I don't get it.", likes: 10},
+  {user: "Michael Myers", message: "I only kill cool teens.", likes: 5},
+  {user: "Girl from the Ring", message: "Watch this cool video", likes: 12},
+]
+
+export default messages
