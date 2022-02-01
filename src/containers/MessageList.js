16a2f1733659d55cb6571bfd6bb98cbfe57c848a
Laurie Roy <laurieroy.dev@gmail.com>
2022-02-01 14:58:58 -0800

    Add counter hook

--- src/containers/MessageList.js
+++ src/containers/MessageList.js
@@ -1,11 +1,14 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/prop-types */
-
+// import {useState} from 'react'
 import Message from '../components/Message'
 
 function MessageList(props){
+    // const [counter, setCounter] = useState(0);
+    // const handleClick = () => setCounter(counter + 1);
 
         return <ol>
+            {/* <button onClick={handleClick}>{counter}</button> */}
             {props.messages.map((m) => <Message key={m.id} message = {m} />)} 
             <button onClick={props.addMsg}>Add message</button>
         </ol>
