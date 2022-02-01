15f77d74926f1d3a84e98dff92c7fa27913f96d9
Laurie Roy <laurieroy.dev@gmail.com>
2022-02-01 14:19:55 -0800

    Refactor moving state up a level for form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
+/* eslint-disable react/react-in-jsx-scope */
 import './App.css';
+import { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 import MessageList from './containers/MessageList';
 
-function App() {
-  title="Welcome to devChat"
-  subtitle = "A place for devs to get help, offer advice and chill"
-  
-  return (
-    <>
-    <h1>{title}</h1>
-    <h3>{subtitle}</h3>
-      <MessageList />
-    </>
-  );
+class App extends Component {
+  // title=""
+  // subtitle = "A place for devs to get help, offer advice and chill"
+  state = { 
+    messages: [
+        {user: "Freddy Kreuger", message: "Starring in a new movie where I murder coders. Calling it Freddy vs. JSON", id: uuidv4()},
+        {user: "Jason Voorhees", message: "I don't get it.", id: uuidv4()},
+        {user: "Michael Myers", message: "I only kill cool teens.", id: uuidv4()},
+        {user: "Girl from the Ring", message: "Watch this cool video", id: uuidv4()},
+    ]
+}
+addMsg = () =>  {
+  this.setState({messages: [...this.state.messages, {user: "god", message: "lol", id: uuidv4() }]})
+}
+
+  render(){
+    return <main>
+      <h1>Welcome to devChat</h1>
+      <h3>A place for devs to meet up, get help, offer advice and chill</h3> 
+      <MessageList messages={this.state.messages} addMsg={this.addMsg} />
+    </main>
+  }
+
+
 }
 
 export default App;
--- src/containers/MessageList.js
+++ src/containers/MessageList.js
@@ -1,28 +1,16 @@
-import React, { Component }  from 'react';
-import { v4 as uuidv4 } from 'uuid';
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/prop-types */
 
 import Message from '../components/Message'
 
-class MessageList extends Component {
-    state = { 
-        messages: [
-            {user: "Freddy Kreuger", message: "Starring in a new movie where I murder coders. Calling it Freddy vs. JSON", id: uuidv4()},
-            {user: "Jason Voorhees", message: "I don't get it.", id: uuidv4()},
-            {user: "Michael Myers", message: "I only kill cool teens.", id: uuidv4()},
-            {user: "Girl from the Ring", message: "Watch this cool video", id: uuidv4()},
-        ]
-    }
+function MessageList(props){
 
-    addMsg = () =>  {
-        this.setState({messages: [...this.state.messages, {user: "god", message: "lol", id: uuidv4() }]})
-    }
-    render() {
         return <ol>
-            {this.state.messages.map((m) => <Message key={m.id} message = {m} />)} 
-            <button onClick={this.addMsg}>Add message</button>
+            {props.messages.map((m) => <Message key={m.id} message = {m} />)} 
+            <button onClick={props.addMsg}>Add message</button>
         </ol>
     
-    }
+    
 }
 
 export default MessageList;
