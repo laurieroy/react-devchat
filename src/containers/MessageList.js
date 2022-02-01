e5d18521c852393cd897734fe58701e9fe21a581
Laurie Roy <laurieroy.dev@gmail.com>
2022-02-01 14:09:51 -0800

    Add uuid

--- src/containers/MessageList.js
+++ src/containers/MessageList.js
@@ -1,25 +1,28 @@
-import { Component } from "react"
+import React, { Component }  from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import Message from '../components/Message'
 
+class MessageList extends Component {
+    state = { 
+        messages: [
+            {user: "Freddy Kreuger", message: "Starring in a new movie where I murder coders. Calling it Freddy vs. JSON", id: uuidv4()},
+            {user: "Jason Voorhees", message: "I don't get it.", id: uuidv4()},
+            {user: "Michael Myers", message: "I only kill cool teens.", id: uuidv4()},
+            {user: "Girl from the Ring", message: "Watch this cool video", id: uuidv4()},
+        ]
+    }
 
-class MessageList extends Component () {
-    state = { messages: [
-        {user: "Freddy Kreuger", message: "Starring in a new movie where I murder coders. Calling it Freddy vs. JSON"},
-        {user: "Jason Voorhees", message: "I don't get it."},
-        {user: "Michael Myers", message: "I only kill cool teens."},
-        {user: "Girl from the Ring", message: "Watch this cool video"},
-      ]}
-      render(){
-
-   
-        return 
-            <ul>
-                {this.messages.map((m, i )=> <Message key={i} message = {m} />)}
-                
-            </ul>
+    addMsg = () =>  {
+        this.setState({messages: [...this.state.messages, {user: "god", message: "lol", id: uuidv4() }]})
+    }
+    render() {
+        return <ol>
+            {this.state.messages.map((m) => <Message key={m.id} message = {m} />)} 
+            <button onClick={this.addMsg}>Add message</button>
+        </ol>
     
-       }
+    }
 }
 
 export default MessageList;
